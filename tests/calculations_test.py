96d1c7585a6c457665a481112618904c6b18ee63
Gowrisankar <95461759+Gowrisankar5877@users.noreply.github.com>
2025-09-18 05:50:17 +0000

    Added testr cases

--- tests/calculations_test.py
+++ tests/calculations_test.py
@@ -46,21 +46,24 @@ def test_get_nth_fibonacci_zero():
     assert result == 0
 
 
-def test_get_nth_fibonacci_one():
-    """Test with n=1."""
-    # Arrange
-    n = 1
 
-    # Act
-    result = get_nth_fibonacci(n)
-
-    # Assert
-    assert result == 1
 
 
 def test_get_nth_fibonacci_ten():
     """Test with n=10."""
     # Arrange
+
+def test_get_nth_fibonacci_zero():
+    """Test with n=0."""
+    # Arrange
+    n = 0
+
+    # Act
+    result = get_nth_fibonacci(n)
+
+    # Assert
+    assert result == 0
+
     n = 10
 
     # Act
